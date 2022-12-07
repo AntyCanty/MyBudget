7bb6f9ae52406e9b31f28c70fe8c1acc67fd20d3
Antimo Cantiello <64530127+AntyCanty@users.noreply.github.com>
2022-12-07 12:00:19 +0100

    First Commit

---
+++ MyBudget/MyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetApp.swift
+//  MyBudget
+//
+//  Created by Antimo Cantiello on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
