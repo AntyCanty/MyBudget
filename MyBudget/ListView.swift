5e0f1ed667063f7c6e785b9214c6814c254797e2
Bocco98 <tony.bocchetti98@gmail.com>
2022-12-09 20:54:31 +0100

    I've done the class article and list, We need do create the article page and to link it to the list page

---
+++ MyBudget/ListView.swift
@@ -0,0 +1,86 @@
+//
+//  ListView.swift
+//  MyBudget
+//
+//  Created by Antonio Bocchetti on 09/12/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @ObservedObject var myList = listData
+    @State private var searchText = ""
+    //    @State var newListViewisPresented: Bool = false
+    
+    
+    
+    var body: some View {
+        
+        NavigationStack{
+            
+            ScrollView{
+                ForEach(searchResults){ lista in
+                    ZStack(alignment: .leading){
+                        
+                        Image(lista.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .shadow(radius: 10)
+                            .frame(height: 100)
+                        
+                        Rectangle()
+                            .fill(LinearGradient(colors: [.clear, .black.opacity(0.8)], startPoint:.top, endPoint: .bottom))
+                            .frame(height: 100)
+                            //.frame(width: 100)
+                            
+                        
+                        VStack(alignment: .leading){
+                            Text(lista.title)
+                                .font(.title)
+                                .bold()
+                        }
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                            
+                            
+                        }
+                    
+                        .cornerRadius(20)
+                        .padding(.horizontal)
+                    }
+                    .onDelete(perform: delete) //IT DOESN'T WORK
+                    
+                }
+            
+            .navigationTitle("Lists")
+            .searchable(text: $searchText)
+        }
+        
+    
+    }
+    
+    var searchResults: [List_] {
+        if searchText.isEmpty {
+            return myList.lists
+        } else {
+            return myList.lists.filter{$0.title.contains(searchText)}
+        }
+    }
+    
+    func delete(at offsets: IndexSet){
+        myList.lists.remove(atOffsets: offsets)
+    }
+}
+
+
+
+
+
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
+
---
+++ MyBudget/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  MyBudget
+//
+//  Created by Antonio Bocchetti on 09/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+//Creiamo il model per un generico articolo
+struct Article: Identifiable {//Identifica l'oggetto come UNICO
+    var id = UUID()
+    var name: String
+    var price: Double
+    var quantity : Double = 1
+    var weight : Double = 0
+}
+
+//Creiamo il model per una generica lista
+struct List_: Identifiable {//Identifica l'oggetto come UNICO
+    var id = UUID()
+    var title: String
+    var budget: Double
+    var articles : [Article]
+    var imageName : String = ""
+}
