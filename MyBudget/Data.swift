0eafaff4dbd18d3cf53cdb513c69bc324af3c030
davit <davit.danelia.1@btu.edu.ge>
2022-12-14 09:50:51 +0100

    Resolving conflicts

--- MyBudget/Data.swift
+++ MyBudget/Data.swift
@@ -30,39 +30,19 @@ var articleData = ArticleData()
 class ListData: ObservableObject{//dico che questa classe puo essere usata in altre views e                                    se cambia qualcosa devo modificare anche le altre views
     
     @Published var lists = [
-        List_(title: "Grocery List", budget: 20, articles: [Article(name: "Bread", price:                                                  1.00, quantity: 1),
-                                                           Article(name: "Milk", price: 1.20, quantity: 0.500),
-                                                           Article(name: "Biscuits", price: 3.00, quantity: 1),
-                                                           Article(name: "Hamburger", price: 5.30),
-                                                           Article(name: "Eggs", price: 2.20,quantity: 1),
-                                                           Article(name: "Apple", price: 3.00)],
-                                                            imageName: "grocery"),
-        
-        List_(title: "Clothes List", budget: 1250, articles: [        Article(name:                                                       "T-Shirt", price: 9.99,quantity: 1),
-                                                                    Article(name: "Shorts", price: 15.00, quantity: 1),
-                                                                    Article(name: "Shoes", price: 80.00, quantity: 1),
-                                                                    Article(name: "Socks", price: 5.00, quantity: 2)],
-                                                              imageName: "clothes"),
-        
-        List_(title: "Grocery List", budget: 20, articles: [Article(name: "Bread", price:                                                  1.00, quantity: 1),
-                                                           Article(name: "Milk", price: 1.20, quantity: 0.500),
-                                                           Article(name: "Biscuits", price: 3.00, quantity: 1),
-                                                           Article(name: "Hamburger", price: 5.30),
-                                                           Article(name: "Eggs", price: 2.20,quantity: 1),
-                                                           Article(name: "Apple", price: 3.00)],
+        List_(title: "Grocery List", budget: 20, articles: [Article(name: "Bread", price:                                                  1.00, quantity: 1, weight: 0.500, brought: false),
+                                                           Article(name: "Milk", price: 1.20, quantity: 0.500, brought: false),
+                                                           Article(name: "Biscuits", price: 3.10, quantity: 1, brought: true),
+                                                           Article(name: "Cream", price: 5.30, weight: 0.700, brought: false),
+                                                           Article(name: "Eggs", price: 2.20,quantity: 1, brought: true),
+                                                           Article(name: "Apple", price: 3.00, weight: 1, brought: false)],
                                                             imageName: "grocery"),
         
         List_(title: "Clothes List", budget: 100, articles: [        Article(name:                                                       "T-Shirt", price: 9.99,quantity: 1),
-                                                                    Article(name: "Shorts", price: 15.00, quantity: 1),
-                                                                    Article(name: "Shoes", price: 80.00, quantity: 1),
-                                                                    Article(name: "Socks", price: 5.00, quantity: 2)],
+                                                                    Article(name: "Shorts", price: 15.00, quantity: 1, brought: false),
+                                                                    Article(name: "Shoes", price: 80.00, quantity: 1, brought: false),
+                                                                    Article(name: "Socks", price: 5.00, quantity: 2, brought: false)],
                                                               imageName: "clothes")
-        
-
-        
-
-     
-
         
   ]
     
