65c94bed677ffc72d0b7de179cd113588077998a
Antimo Cantiello <antimo.ca98@gmail.com>
2022-12-15 11:00:58 +0100

    Fix pos in FavouriteCard

--- MyBudget/FavouritesCardView.swift
+++ MyBudget/FavouritesCardView.swift
@@ -10,31 +10,36 @@ import SwiftUI
 struct FavouritesCardView: View {
     
     let columns = [
-        GridItem(.fixed(165)),
-        GridItem(.fixed(160))
+        GridItem(.fixed(155)),
+        GridItem(.fixed(150))
     ]
     
     @ObservedObject var myList = listData
     
     var body: some View {
         
-            ZStack(alignment: .top){
-                Rectangle()
-                    .padding(.bottom)
-                    .frame(width: 350, height: 415)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.white)
-                    .opacity(0.07)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 16)
-                            .stroke(Color(hue: 0.818, saturation: 0.636, brightness: 0.431), lineWidth: 4))
+        ZStack(alignment: .top){
+            Rectangle()
+                .padding(.bottom)
+                .frame(width: 350, height: 415)
+                .cornerRadius(10)
+                .foregroundColor(Color.white)
+                .opacity(0.07)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color(hue: 0.818, saturation: 0.636, brightness: 0.431), lineWidth: 4))
+            
+            
+            
+            Text("Favourite Lists")
+                .bold()
+                .frame(width: 240, height: 20, alignment: .top)
+                .padding(.top,10)
+
                 
-                Text("Favourite Lists")
-                    .bold()
-                    .frame(width: 200, height: 20, alignment: .top)
-                    .padding(.top,10)
-                ScrollView{
-                LazyVGrid(columns: columns, spacing: 0){
+            
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 20){
                     
                     ForEach(myList.lists){ lista in
                         NavigationLink(destination:ListDetailView(lista: lista)){
@@ -48,6 +53,7 @@ struct FavouritesCardView: View {
             }
             //.padding(.top,200)
         }
+        .frame(height: 415)
     }
 }
 
